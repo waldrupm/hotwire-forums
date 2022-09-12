a1c12e80da3216e8e1b2d3a303d91ec5f48916b3
Micheal Waldrup <waldrupm@gmail.com>
2022-09-12 13:25:27 -0400

    add inline editing and delete, create small bug

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,10 +1,12 @@
 class DiscussionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_discussion, only: %i[edit update destroy]
+  before_action :set_discussion, only: %i[show edit update destroy]
 
   def index
     @discussions = Discussion.all
   end
+
+  def show; end
 
   def new
     @discussion = Discussion.new
@@ -15,7 +17,7 @@ class DiscussionsController < ApplicationController
 
     respond_to do |format|
       if @discussion.save
-        format.html { redirect_to discussions_path, notice: 'Discussion created' }
+        format.html { redirect_to @discussion, notice: 'Discussion created' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -27,7 +29,7 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
-        format.html { redirect_to discussions_path, notice: 'Discussion updated' }
+        format.html { redirect_to @discussion, notice: 'Discussion updated' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -35,8 +37,8 @@ class DiscussionsController < ApplicationController
   end
 
   def destroy
-    @discussion.destroy!
-    redirect_to discussions_path, notice: "Discussion deleted"
+    @discussion.destroy
+    redirect_to discussions_path, notice: 'Discussion deleted', status: :see_other
   end
 
   private
